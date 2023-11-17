0fcee02b41a9016e288c9d8dc2b59f1263e501bd
yazzy14 <yazminbazail@icloud.com>
2023-11-16 21:14:07 -0800

    added if statement for the dy of the week

--- main.js
+++ main.js
@@ -15,25 +15,24 @@ Afternoon (12 noon to 6pm)
 Evening (6pm to 11pm)
 Night (11pm to 5am)
 */
+
 let myDate = new Date();
 let myHour = myDate.getHours();
+let day = myDate.getDay();
 let myGreeting = "";
 let myColor = "";
 let myPic = "";
+let todayIs = day;
 
-// put in place for testin - remove line below
-
-myHour = 1;
-
-if (myHour < 12) {
+if (myHour >= 5 && myHour < 12) {
   myGreeting = "Good Morning!";
   myColor = "LightYellow";
   myPic = "morning.jpg";
-} else if (myHour >= 12) {
+} else if (myHour >= 12 && myHour < 18) {
   myGreeting = "Good Afternoon!";
   myColor = "LightBlue";
   myPic = "afternoon.jpg";
-} else if (myHour >= 18) {
+} else if (myHour >= 18 && myHour < 23) {
   myGreeting = "Good Evening!";
   myColor = "Blue";
   myPic = "evening.jpg";
@@ -43,9 +42,27 @@ if (myHour < 12) {
   myPic = "night.jpg";
 }
 
+//Can also use switch statement for each number
+if (day == 0) {
+  todayIs = "Today is Sunday";
+} else if (day == 1) {
+  todayIs = "Today is Monday";
+} else if (day == 2) {
+  todayIs = "Today is Tuesday";
+} else if (day == 3) {
+  todayIs = "Today is Wednesday";
+} else if (day == 4) {
+  todayIs = "Today is Thursday";
+} else if (day == 5) {
+  todayIs = "Today is Friday";
+} else {
+  todayIs = "Today is Saturday";
+}
+
 myPic = "images/" + myPic;
 
 // Custom Message
 document.getElementById("my-greeting").innerHTML = myGreeting;
 document.querySelector("html").style.backgroundColor = myColor;
 document.getElementById("my-pic").src = myPic;
+document.getElementById("today").innerHTML = todayIs;
